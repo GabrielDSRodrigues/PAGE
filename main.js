025c3d081f1483774a3670b89060e8ca7b758eeb
GabrielDSRodrigues <gabrielrodrigues.sp.br@gmail.com>
2021-05-30 00:42:35 -0300

    Page atualização

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-function id(e) {return document.getElementById(e);}
-function go(href) {location.href = "projects/" + href;}
+function id(e) { return document.getElementById(e); }
+
+function go(href) { location.href = href; }
 
 function add(title, text, img, href) {
   id("panel").insertAdjacentHTML("afterbegin", `
@@ -7,7 +8,22 @@ function add(title, text, img, href) {
       <div style="background: url(pictures/${img}); background-size: cover;" class="picture"></div>
       <p class="title">${title}</p>
       <p class="text">${text}</p>
-      <button onclick="go('${href}')"class="btn">Ver</button>
+      <button onclick="go('projects/${href}')"class="btn">Ver</button>
     </div>
   `);
 }
+
+function click(e, f) {
+  e.addEventListener("click", () => { f(); });
+}
+
+let projects = id("projects");
+let Activities = id("activities");
+
+click(projects, () => {
+  go("index.html");
+});
+
+click(activities, () => {
+  go("activities.html");
+});
