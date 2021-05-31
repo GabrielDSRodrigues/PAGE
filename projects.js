f0088ca4e9b40dca6155cbecc3ec9fc2c242009d
GabrielDSRodrigues <gabrielrodrigues.sp.br@gmail.com>
2021-05-30 23:55:55 -0300

    Adição do projeto da aula de DDW da ETEC

--- projects.js
+++ projects.js
@@ -4,3 +4,10 @@ projectAdd(
   "login-form.jpg",
   "login-form/"
 );
+
+projectAdd(
+  "DDW | Atividade 01",
+  "Atividade do curso de Informática da ETEC | Um cartão contendo dados.",
+  "ddw-01.jpg",
+  "DDW/ATIVIDADE 01/"
+);
